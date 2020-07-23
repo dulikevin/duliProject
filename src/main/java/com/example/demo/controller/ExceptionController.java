7f1d1f218e8bff3f94d9481cb05f493818c9de72
duliKevin <1751949926@qq.com>
2020-07-23 15:52:27 +0800

    修改showException类。

--- src/main/java/com/example/demo/controller/ExceptionController.java
+++ src/main/java/com/example/demo/controller/ExceptionController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.domain.MyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
